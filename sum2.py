32e85c6633eaa82dd5bb98cbe6d4ef9f5fcfcdd2
Samarth Ghodake <85952893+samarth56@users.noreply.github.com>
2024-08-04 02:17:31 +0530

    Update sum2.py

--- sum2.py
+++ sum2.py
@@ -1,57 +1,79 @@
-import streamlit as st
-from txtai.pipeline import Summary, Textractor
-from PyPDF2 import PdfReader
+import streamlit as st 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.chains.summarize import load_summarize_chain
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import pipeline
+import torch
+import base64
 
-st.set_page_config(layout="wide")
+#model and tokenizer loading
+checkpoint = "LaMini-Flan-T5-248M"
+tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
 
-@st.cache_resource
-def text_summary(text, maxlength=None):
-    #create summary instance
-    summary = Summary()
-    text = (text)
-    result = summary(text)
+#file loader and preprocessing
+def file_preprocessing(file):
+    loader =  PyPDFLoader(file)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    texts = text_splitter.split_documents(pages)
+    final_texts = ""
+    for text in texts:
+        print(text)
+        final_texts = final_texts + text.page_content
+    return final_texts
+
+#LLM pipeline
+def llm_pipeline(filepath):
+    pipe_sum = pipeline(
+        'summarization',
+        model = base_model,
+        tokenizer = tokenizer,
+        max_length = 500, 
+        min_length = 50)
+    input_text = file_preprocessing(filepath)
+    result = pipe_sum(input_text)
+    result = result[0]['summary_text']
     return result
 
-def extract_text_from_pdf(file_path):
-    # Open the PDF file using PyPDF2
-    with open(file_path, "rb") as f:
-        reader = PdfReader(f)
-        page = reader.pages[0]
-        text = page.extract_text()
-    return text
+@st.cache_data
+#function to display the PDF of a given file 
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
 
-if choice == "Summarize Text":
-    st.subheader("Summarize Text using txtai")
-    input_text = st.text_area("Enter your text here")
-    if input_text is not None:
-        if st.button("Summarize Text"):
-            col1, col2 = st.columns([1,1])
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+#streamlit code 
+st.set_page_config(layout="wide")
+
+def main():
+    st.title("Document Summarization App using Langauge Model")
+
+    uploaded_file = st.file_uploader("Upload your PDF file", type=['pdf'])
+
+    if uploaded_file is not None:
+        if st.button("Summarize"):
+            col1, col2 = st.columns(2)
+            filepath = "data/"+uploaded_file.name
+            with open(filepath, "wb") as temp_file:
+                temp_file.write(uploaded_file.read())
             with col1:
-                st.markdown("**Your Input Text**")
-                st.info(input_text)
-            with col2:
-                st.markdown("**Summary Result**")
-                result = text_summary(input_text)
-                st.success(result)
+                st.info("Uploaded File")
+                pdf_view = displayPDF(filepath)
 
-elif choice == "Summarize Document":
-    st.subheader("Summarize Document using txtai")
-    input_file = st.file_uploader("Upload your document here", type=['pdf'])
-    if input_file is not None:
-        if st.button("Summarize Document"):
-            with open("doc_file.pdf", "wb") as f:
-                f.write(input_file.getbuffer())
-            col1, col2 = st.columns([1,1])
-            with col1:
-                st.info("File uploaded successfully")
-                extracted_text = extract_text_from_pdf("doc_file.pdf")
-                st.markdown("**Extracted Text is Below:**")
-                st.info(extracted_text)
             with col2:
-                st.markdown("**Summary Result**")
-                text = extract_text_from_pdf("doc_file.pdf")
-                doc_summary = text_summary(text)
-                st.success(doc_summary)
-                
+                summary = llm_pipeline(filepath)
+                st.info("Summarization Complete")
+                st.success(summary)
+
+
+
+if __name__ == "__main__":
+    main()
